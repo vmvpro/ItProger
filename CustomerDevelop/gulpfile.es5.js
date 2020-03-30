a4d5cc630fdf9258699137d7d09b8db07596f308
vmvpro <melnvetal@gmail.com>
2020-03-31 00:24:09 +0300

    Обновленный файл gulpFile.js

--- CustomerDevelop/gulpfile.es5.js
+++ CustomerDevelop/gulpfile.es5.js
@@ -1,60 +1,40 @@
-﻿/// <binding />
-//'use strict';
-
-//var gulp = require('gulp');
-//var	sass = require('gulp-sass'); // добавляем модуль sass
-//var uglifycss = require('gulp-uglifycss'); // добавляем модуль sass
-
-////// регистрируем задачу для конвертации файла scss в css
-
-//var gulp_sass = gulp.task('sass', function () {
-//	return gulp.src('./scss/*.scss')
-//		.pipe(sass())
-//		.pipe(gulp.dest('./dist/css'));
-//});
-
-//var gulp_css = gulp.task('css', function () {
-//	return gulp.src('./scss/*.css')
-//		.pipe(uglifycss({
-//			'uglyComments': true
-//		}))
-//		.pipe(gulp.dest('./dist/css'));
-//});
-//----------------------------------------------------------
-
-'use strict';
+﻿'use strict';
 
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass');
-var uglifycss = require('gulp-uglifycss'); // добавляем модуль sass
 
-var _sass = function _sass(done) {
+var functionSass = function functionSass(done) {
+
+	// Папка, где сохраняются файлы *.scss
+	// (ProjectCustomerDevelop/scss)
 	gulp.src("scss/*.scss")
-	//.pipe(sass({ outputStyle: 'compressed' }))
-	.pipe(sass({ outputStyle: 'expanded' })).pipe(gulp.dest("dist/css")).pipe(browserSync.stream());
+
+	//параметр для минификации - compressed
+	.pipe(sass({ outputStyle: 'expanded' }))
+
+	//Расположение преобразованных файлов *.css
+	.pipe(gulp.dest("dist/css"))
+
+	// соединение веб-браузера
+	.pipe(browserSync.stream());
 
 	done();
 };
 
-//gulp.task('sass', function (done) {
-//	gulp.src("scss/*.scss")
-//		//.pipe(sass({ outputStyle: 'compressed' }))
-//		.pipe(sass({ outputStyle: 'expanded' }))
-//		.pipe(gulp.dest("dist/css"))
-//		.pipe(browserSync.stream());
-
-//	done();
-//});
-
-var _serve = function _serve(done) {
+var functionService = function functionService(done) {
 
 	browserSync.init({ server: "" }
-	//{ notify: true } // отклчение уведомлений
+
+	// отклчение уведомлений сервера,
+	//{ notify: true }
 	);
 
-	//gulp.watch("scss/*.scss", gulp.series('sass'));
-	gulp.watch("scss/*.scss", gulp.series(_sass));
+	// Подключение слежение за файлами
+	gulp.watch("scss/*.scss", gulp.series(functionSass));
+
+	// В данном случае, файл в корневом
+	// размецении проекта
 	gulp.watch("*.html").on('change', function () {
 		browserSync.reload();
 		done();
@@ -63,86 +43,7 @@ var _serve = function _serve(done) {
 	done();
 };
 
-//gulp.task('serve', function (done) {
-
-//	browserSync.init(
-//		{ server: "" }
-//		//{ notify: true } // отклчение уведомлений
-//	);
-
-//	gulp.watch("scss/*.scss", gulp.series('sass'));
-//	gulp.watch("*.html").on('change', () => {
-//		browserSync.reload();
-//		done();
-//	});
-
-//	done();
-//});
-
-//gulp.task('default', gulp.series('sass', 'serve'));
-gulp.task('default', gulp.series(_sass, _serve));
-
-//---------------------------------------------------
-
-//var cleanCSS = require('gulp-clean-css');
-//var rename = require('gulp-rename');
-
-//gulp.task('minify-css', function () {
-//	return gulp.src('css/dist/dist.css')
-//		.pipe(cleanCSS())
-//		.pipe(rename('dist.min.css'))
-//		.pipe(gulp.dest('css/dist'));
-//});
-
-//gulp.task('css', function () {
-//	gulp.src('./styles/**/*.css')
-//		.pipe(uglifycss())
-//		.pipe(gulp.dest('./dist/'));
-//});
-
-//gulp.task('css', function () {
-//	gulp.src('css/*.css')
-//		.pipe(uglifycss({
-//			"maxLineLen": 80,
-//			"uglyComments": true
-//		}))
-//		.pipe(gulp.dest('./dist/'));
-//});
-
-//----------------------------------------------------
-
-//gulp.series('default', gulp.parallel(gulp_sass, gulp_css));
-
-//gulp.task('run', ['sass', 'css']);
-
-//gulp.task('watch',
-//	function () {
-//		gulp.watch('./scss/*.scss', ['sass']);
-//		gulp.watch('./css/*.css', ['css']);
-//	});
-
-//gulp.task('default', ['run', 'watch']);
-
-//---------------------------------------------------
-
-//gulp.task('css:watch', function () {
-//	gulp.watch('./dist/', ['css']);
-//});
-
-//gulp.task('css:watch_', function () {
-//	gulp.watch('./dist', ['css']);
-//});
-
-//gulp.task('default', gulp.parallel(styles, scripts));
-
-//gulp.watch('./dist', ['css']);
-
-//gulp.series('css:watch_', function () {
-//	gulp.watch('./dist', ['css']);
-//});
-
-//gulp.task("watch",
-//	function() {
-//		'scss/style.sccs', gulp.parallel('sass');
-//	});
+// Создание задачи в Visual Studio
+// Tasks Manager (Диспетчер выполнения задач)
+gulp.task('run_service', gulp.series(functionSass, functionService));
 
