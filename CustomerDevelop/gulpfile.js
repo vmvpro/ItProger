6fb1ffee8caf1970eb20b076f21f7f4544ceea5a
vmvpro <melnvetal@gmail.com>
2020-03-30 17:21:28 +0300

    Произведена синхронизация gulp Осталось сделать минификацию

--- CustomerDevelop/gulpfile.js
+++ CustomerDevelop/gulpfile.js
@@ -1,25 +1,61 @@
 ﻿/// <binding />
-'use strict';
+//'use strict';
+
+//var gulp = require('gulp');
+//var	sass = require('gulp-sass'); // добавляем модуль sass
+//var uglifycss = require('gulp-uglifycss'); // добавляем модуль sass
+
+////// регистрируем задачу для конвертации файла scss в css
+
+//var gulp_sass = gulp.task('sass', function () {
+//	return gulp.src('./scss/*.scss')
+//		.pipe(sass())
+//		.pipe(gulp.dest('./dist/css'));
+//});
+
+//var gulp_css = gulp.task('css', function () {
+//	return gulp.src('./scss/*.css')
+//		.pipe(uglifycss({
+//			'uglyComments': true
+//		}))
+//		.pipe(gulp.dest('./dist/css'));
+//});
+//----------------------------------------------------------
 
 var gulp = require('gulp');
-var	sass = require('gulp-sass'); // добавляем модуль sass
+var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
 var uglifycss = require('gulp-uglifycss'); // добавляем модуль sass
 
-//// регистрируем задачу для конвертации файла scss в css
+gulp.task('sass', function (done) {
+	gulp.src("scss/*.scss")
+		.pipe(sass({ outputStyle: 'compressed' }))
+		.pipe(gulp.dest("dist/css"))
+		.pipe(browserSync.stream());
 
-var gulp_sass = gulp.task('sass', function () {
-	return gulp.src('./scss/*.scss')
-		.pipe(sass())
-		.pipe(gulp.dest('./dist/css'));
+	done();
 });
 
-var gulp_css = gulp.task('css', function () {
-	return gulp.src('./scss/*.css')
-		.pipe(uglifycss({
-			'uglyComments': true
-		}))
-		.pipe(gulp.dest('./dist/css'));
+gulp.task('serve', function (done) {
+
+	browserSync.init(
+		{
+			server: ""
+		},
+		{
+			notify: false // отклчение уведомлений
+		});
+
+	gulp.watch("scss/*.scss", gulp.series('sass'));
+	gulp.watch("*.html").on('change', () => {
+		browserSync.reload();
+		done();
+	});
+
+	done();
 });
+
+gulp.task('default', gulp.series('sass', 'serve'));
 
 //gulp.series('default', gulp.parallel(gulp_sass, gulp_css));
 
