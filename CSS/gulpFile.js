4172e898ce1f493f08faac1da4bfd4f70cf77918
vmvpro <melnvetal@gmail.com>
2020-03-31 21:04:26 +0300

    fix

--- CSS/gulpFile.js
+++ CSS/gulpFile.js
@@ -37,6 +37,11 @@ paths.concatCssMapDest = paths.webroot + "css/*.min.map";
 //	rimraf(paths.concatJsDest, done);
 //});
 
+function cssCleanFunc(done) {
+	rimraf("source/scss/*.min.css", done);
+	rimraf("source/scss/*.map", done);
+};
+
 var cssClean = function (done) {
 	rimraf("source/scss/*.min.css", done);
 	rimraf("source/scss/*.map", done);
@@ -52,15 +57,15 @@ var cssClean = function (done) {
 
 //----------------------------------------------------------------
 
-var cssMin = function () {
-	return gulp.src(["source/dist/css/*.css", "!source/dist/css/*.min.css"])
+var cssMin = function cssMinFunc () {
+	return gulp.src(["source/css/*.css", "!source/css/*.min.css"])
 		.pipe(concat("source/css/" + nameFileCss + ".min.css"))
 		.pipe(cssmin())
 		.pipe(gulp.dest("."))
-		
+
 		// соединение веб-браузера
 		//.pipe(browserSync.stream())
-	;
+		;
 };
 
 
@@ -83,6 +88,7 @@ var cssMin = function () {
 //	gulp.series("min:css", "min:js"));
 
 //----------------------------------------------------------------
+
 var functionSass = function (done) {
 
 	// Папка, где сохраняются файлы *.scss 
@@ -93,24 +99,22 @@ var functionSass = function (done) {
 		.pipe(sass({ outputStyle: 'expanded' }))
 
 		//Расположение преобразованных файлов *.css 
-		.pipe(gulp.dest("source/dist/css"))
+		.pipe(gulp.dest("source/css"))
 
 		// соединение веб-браузера
 		.pipe(browserSync.stream());
 
-	//gulp.log("sdasd");
-	log("vmv");
-	gulp.series(cssClean);
+	cssCleanFunc(done);
 
 	done();
+
+	log("after functionSass done()");
 }
 
-var file_ = function () {
-
-};
 
 var functionService = function (done) {
 
+	log("befor functionService");
 	browserSync.init({
 		//server: {
 		//	baseDir: "./"
@@ -137,14 +141,20 @@ var functionService = function (done) {
 		//{ notify: true } 
 	);
 
+	log("after functionService");
+
 	// В данном случае, файл в корневом 
 	// размецении проекта
-	
+
 
 	// Подключение слежение за файлами
 	gulp.watch(
 		"source/scss/*.scss",
-		gulp.series(functionSass, cssMin, cssClean));
+		//gulp.series(Sass(done)));
+		gulp.series(functionSass))
+		.pipe()
+		;
+
 
 	//minCss,
 
@@ -152,7 +162,7 @@ var functionService = function (done) {
 	//	gulp.series(minCss));
 
 	gulp.watch(
-			"*.html")
+		"*.html")
 		.on('change', () => {
 			browserSync.reload("");
 			//gulp.series(cssMin);
@@ -160,10 +170,13 @@ var functionService = function (done) {
 		});
 
 	done();
+
+	log("befor functionSass done()");
 }
 
 // Создание задачи в Visual Studio
 // Tasks Manager (Диспетчер выполнения задач)
+//, cssMin, cssClean
 gulp.task('run_service',
-	gulp.series(functionSass, functionService, cssMin, cssClean));
+	gulp.series(functionSass, functionService));
 
