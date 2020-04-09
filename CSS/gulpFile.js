9a4112a3ab91e28f022d9a3db4e70bf89f0b1c2f
vmvpro <melnvetal@gmail.com>
2020-04-09 23:15:59 +0300

    Создание адаптивной формы

--- CSS/gulpFile.js
+++ CSS/gulpFile.js
@@ -18,7 +18,7 @@ var log = require('fancy-log');
 var number = "06";
 
 var htmlFile = {
-	Name: "index_06_BlockPresentation.html"
+	Name: "index_06_FeedbackForm.html"
 };
 
 var nameFileCss = "style" + number;
