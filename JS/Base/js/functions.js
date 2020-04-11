2e86dbe0f914d559179084868e5df46853c044a4
vmvpro <melnvetal@gmail.com>
2020-04-11 21:23:42 +0300

    Основы JavaScript

--- JS/Base/js/functions.js
+++ JS/Base/js/functions.js
@@ -1,8 +1,70 @@
 ﻿var name = "vmv";
 
-document.write("<p>Hello VMVPRO!!!</p>");
-document.write("<p>_99Hello Hello еще раз!!!!!!!!!</p>");
+//document.write("<p>Hello VMVPRO!!!</p>");
+//document.write("<p>_99Hello Hello еще раз!!!!!!!!!</p>");
 
+//var today = new Date();
+
+//document.write("today = " + today.getDate());
+
+//document.write(document.URL + "<br />");
+
+//document.write(screen.availWidth + " - " + screen.availHeight);
+
+//var name = prompt("Ваше Имя!!!");
+//document.write("<p>Hello, " + name + "!!!</p>");
+
+//var digit = prompt("Введите число");
+document.write("<p>")
+//-----------------------------------
+
+//var array = ["vmv", "mmm", "pro"];
+
+//var array = ["a", "b", "c", "d", "e"];
+
+//for (var i = 0; i < array.length; i++) {
+//	p(array[i]);
+//}
+
+//p(array.pop());
+
+//for (var i = 0; i < array.length; i++) {
+//	p(array[i]);
+//}
+
+//var deleteElements = array.splice(3, 3);
+
+//document.write("<br />")
+
+//for (var i = 0; i < deleteElements.length; i++) {
+//	p(deleteElements[i]);
+//}
+
+//document.write("<br />")
+
+//for (var i = 0; i < array.length; i++) {
+//	p(array[i]);
+//}
+
+//document.write(Math.pow(digit, 3));
+
+
+var _parseInt = parseInt("ssss");
+
+var sum = 1 + _parseInt.get;
+
+p(sum);
+
+
+
+
+//-----------------------------------
+
+function p(str) {
+	document.write("<p>" + str + "</p>");
+}
+
+document.write("</p>")
 
 function MyFirstFunction() {
 	document.write("<p>Hello from MyFirstFunction!");
