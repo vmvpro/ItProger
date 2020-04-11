6e0ca666c4c11da992164659fc0753e2a9333362
vmvpro <melnvetal@gmail.com>
2020-04-12 00:20:32 +0300

    Работа со стилями JavaScript + CSS

--- JS/Base/js/functions_HTML.js
+++ JS/Base/js/functions_HTML.js
@@ -10,3 +10,47 @@ function formSend() {
 }
 
 
+function btnStyle() {
+
+	var txt = document.getElementById("txtHide");
+	
+	var myButton = document.getElementById("myButton");
+
+	if (txt.value == 1) {
+	
+		myButton.style.fontSize = "15px";
+		myButton.style.fontFamily = "Comic Sans MS";
+		myButton.style.color = "blue";
+
+		myButton.style.cssText = "margin: 0; padding: 0;";
+
+		txt.value = 2;
+	} else if (txt.value == 2) {
+
+		myButton.style.cssText =
+			"margin: 5px 15px; " +
+			"padding: 5px 15px; " +
+			"border-radius: 5px; " +
+			"border-style: groove;";
+		
+		txt.value = 1;
+
+	}
+
+	
+
+	
+
+
+}
+
+function btnStyle2() {
+	// Выбираем нужный элемент
+	var textChange = document.getElementById("btnStyle");
+	// Меняем размер шрифта
+	textChange.style.fontSize = "15px";
+	// Меняем шрифт
+	
+	// Меняем цвет
+	textChange.style.color = "blue";
+}
