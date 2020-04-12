797b61a43bb3fdd782103c54fe58ca99b6377c77
vmvpro <melnvetal@gmail.com>
2020-04-12 22:49:03 +0300

    Работа с gulpFile.js

---
+++ Gulp/gulpFile.js
@@ -0,0 +1,20 @@
+﻿//exports.myVMV = myVMV;
+
+var gulp = require("gulp");
+var rename = require("gulp-rename");
+//var sass_ = require('gulp-sass');
+
+
+// Задача на копирование css-файлов из преобразования scss
+// Копирование будет происходить из папки scss в css
+function CopyCss(done) {
+
+	gulp.src("./scss/style.scss")
+		.pipe(rename("main.css"))
+		.pipe(gulp.dest("./css"));
+
+	done();
+}
+
+gulp.task(functionSass);
+
